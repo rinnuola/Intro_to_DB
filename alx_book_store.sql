78d8ea434e2123eaf0902e3e6cded0a2d88d718d
Aderinola <aderinolaanna@gmail.com>
2025-10-26 09:12:52 +0100

    made changes to the file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,13 +4,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 -- USE DATABASE
 USE alx_book_store;
 
--- CREATE TABLE AUTHORS
+-- CREATE TABLE authors
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(255) NOT NULL
 );
 
--- CREATE TABLE BOOKS
+-- CREATE TABLE books
 CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -20,15 +20,15 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- CREATE TABLE CUSTOMERS
+-- CREATE TABLE customers
 CREATE TABLE IF NOT EXISTS customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255),
     address VARCHAR(255)
 );
 
--- CREATE TABLE ORDERS
+-- CREATE TABLE orders
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -36,9 +36,9 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- CREATE TABLE ORDER_DETAILS
+-- CREATE TABLE order_details
 CREATE TABLE IF NOT EXISTS order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetails_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity INT,
