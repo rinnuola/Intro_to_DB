cea8f6a38a10073b09aa7f9b14a4ca68065de687
Aderinola <aderinolaanna@gmail.com>
2025-10-28 17:51:00 +0100

    Add changes

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,11 @@
-SELECT INFORMATION_SCHEMA.COLUMNS.COLUMN_NAME, COLUMN_TYPE, TABLE_SCHEMA, TABLE_NAME, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'ala_book_store' AND TABLE_NAME = 'Customers';
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE "COLUMN_NAME" = "COLUMN_NAME" 
+AND "COLUMN_TYPE" = "COLUMN_TYPE" 
+AND "TABLE_SCHEMA" = "alx_book_store" 
+AND "TABLE_NAME" = "Customers";
+
+INSERT INTO customer VALUES 
 (customer_id = 2, customer_name = 'Blessing Malik', email = 'bmalik@sandtech.com', address = '124 Happiness Ave.'),
-(customer_id = 3, customer_name = 'Obed Ihoneah', email = 'eobe@sandtech.com', address = '125 Happiness Ave.'),
-(customer_id = 4, customer_name = 'Nnehanial Kanoli', email = 'nkanoli@sandtech.com', address = '126 Happiness Ave.');
+(customer_id = 3, customer_name = 'Obed Ehoneah', email = 'oebed@sandtech.com', address = '125 Happiness Ave.'),
+(customer_id = 4, customer_name = 'Nehemiah Kamolu', email = 'nkamolu@sandtech.com', address = '126 Happiness Ave.');
