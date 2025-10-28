fb719e47915bccec7a04c49735eeda02d725bf16
Aderinola <aderinolaanna@gmail.com>
2025-10-28 17:59:58 +0100

    Add changes

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,7 @@
-SELECT * 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-AND TABLE_NAME = 'Customers';
+-- Insert multiple rows into the customer table
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'oebed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
